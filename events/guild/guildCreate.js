63b158f7abb66b1aba4a0bb1f78696e03ce25f4b
TheExtremezCoder <156786126+TheExtremezCoder@users.noreply.github.com>
2024-04-11 20:39:06 +0530

    Add files via upload

---
+++ events/guild/guildCreate.js
@@ -0,0 +1,13 @@
+module.exports = async (client, guild) => {
+  let channel = guild.channels.cache.find(
+    channel =>
+    channel.type === "text" &&
+    channel.permissionsFor(guild.me).has("SEND_MESSAGES")
+  );
+  channel.send(`**Thank you for adding me! :white_check_mark:**
+     \`-\` My prefix here is \`!\`
+     \`-\` You can see a list of commands by typing \`!help\` / \`!commands\`
+     \`-\` You can change my prefix with \`!settings prefix <New Prefix>\`
+     \`-\` If you need help, feel free to join our support server at **https://discord.gg/7cRESnhZUm**`);
+  
+}
